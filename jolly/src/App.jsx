d11b28505e18e68a75ebb18069a60a3f42680cbd
Ramesh   Devkota <drhsemar@gmail.com>
2024-10-05 12:35:43 -0500

    Rules of reactjs

--- jolly/src/App.jsx
+++ jolly/src/App.jsx
@@ -1,9 +1,11 @@
+import JSXRules from "./components/JSXRules";
 import WelcomeMessage from "./components/WelcomeMessage";
 
 function App() {
   return (
     <div>
       <WelcomeMessage />
+      <JSXRules />
     </div>
   );
 }
---
+++ jolly/src/components/JSXRules.jsx
@@ -0,0 +1,19 @@
+function JSXRules() {
+  return (
+    <div>
+      <h1>JSX Rules are :--</h1>
+      <p>
+        <ol>
+          <li>JSX must return a single parent element</li>
+          <li>JSXelement must be properly closed </li>
+
+          <li>
+            JSX attributes are written using camelCasd (eg. className instead of
+            class and 'htmlFor' for 'for' attribute)
+          </li>
+        </ol>
+      </p>
+    </div>
+  );
+}
+export default JSXRules;

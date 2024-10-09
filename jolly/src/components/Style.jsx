f8257a73ac87671fd074cee621e15477ba9fded8
Ramesh   Devkota <drhsemar@gmail.com>
2024-10-09 16:02:39 -0500

    React styling added

---
+++ jolly/src/components/Style.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+let Style = { color:"green ", backgroundColor:"black"};
+const Style = () => {
+  return (
+    <h1 {Style}>This is React js styling done here </h1>
+     
+  );
+};
+
+export default Style;
